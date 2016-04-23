5895f753d01d0d7ed85c69082a56821e0324a182
Chen <chenj140@riveter12-ml.att.net>
2016-04-22 22:01:09 -0700

    Created and completed application-form.html page

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index_page():
+    """Show an index page."""
+
+    return "<html><body>This is the homepage.</body></html>"
+
+    # Alternately, we could make this a Jinja template in `templates/`
+    # and return that result of rendering this, like:
+    #
+    # return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
