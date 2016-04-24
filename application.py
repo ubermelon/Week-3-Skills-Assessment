4baf177295efcdc9e79ddbd8a8053246f8981860
ubermelon <ubermelonengineer@gmail.com>
2016-04-23 18:55:46 -0700

    Completed Flask/HTML/CSS skill assessment Week3

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+import jinja2
 
 app = Flask(__name__)
 
@@ -14,6 +15,20 @@ def index_page():
     #
     # return render_template("index.html")
 
+@app.route("/application-form")
+def apply():
+
+	return render_template("application-form.html")
+
+@app.route("/application", methods=["POST"])
+def response_page():
+
+	fname = request.form.get("fname")
+	lname = request.form.get("lname")
+	salary = request.form.get("salary")
+	position = request.form.get("position")
+
+	return render_template("application-response.html", fname=fname, lname=lname, salary=salary, position=position)
 
 if __name__ == "__main__":
     app.run(debug=True)
